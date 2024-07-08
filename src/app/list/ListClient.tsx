0ed9bfdd54e40a97cc9c8d48abfc752f4be8494d
hmatsu47 <35142774+hmatsu47@users.noreply.github.com>
2024-07-08 13:48:43 +0900

    Modify ListClient

--- src/app/list/ListClient.tsx
+++ src/app/list/ListClient.tsx
@@ -45,7 +45,7 @@ export default function ListClient({ isAdminToken }: { isAdminToken: string }) {
       setIsLoading(false);
       loadInitialHaikusAndSetting();
     }
-  }, [isAdminToken, router]);
+  }, [isAdminToken]);
 
   useEffect(() => {
     const filtered = haikus.filter((haiku) => {
